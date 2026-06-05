59ddd9428f76d8279624b8c73946307a6f6f2bc3
Daniel Lavrushin <lavrushin@gmail.com>
2026-06-06 01:15:32 +0200

    fix(parseOrigDst): replace unsafe.Sizeof with unix.Sizeof for length checks

--- src/tproxy/transparent_udp.go
+++ src/tproxy/transparent_udp.go
@@ -95,7 +95,7 @@ func parseOrigDst(oob []byte, v6 bool) (*net.UDPAddr, error) {
 	for _, m := range msgs {
 		if v6 {
 			if m.Header.Level == unix.IPPROTO_IPV6 && m.Header.Type == unix.IPV6_ORIGDSTADDR {
-				if len(m.Data) < int(unsafe.Sizeof(unix.RawSockaddrInet6{})) {
+				if len(m.Data) < unix.SizeofSockaddrInet6 {
 					continue
 				}
 				sa := (*unix.RawSockaddrInet6)(unsafe.Pointer(&m.Data[0]))
@@ -108,7 +108,7 @@ func parseOrigDst(oob []byte, v6 bool) (*net.UDPAddr, error) {
 			continue
 		}
 		if m.Header.Level == unix.IPPROTO_IP && m.Header.Type == unix.IP_ORIGDSTADDR {
-			if len(m.Data) < int(unsafe.Sizeof(unix.RawSockaddrInet4{})) {
+			if len(m.Data) < unix.SizeofSockaddrInet4 {
 				continue
 			}
 			sa := (*unix.RawSockaddrInet4)(unsafe.Pointer(&m.Data[0]))
